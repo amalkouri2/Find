475a004de64118dc403f0a54836a9dc0bd048357
amalkouri2 <amalkouri20@gmail.com>
2025-11-30 00:12:45 +0300

    Create Home component for main page

---
+++ app/Page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Find App is Running 🚀</h1>
+      <p>Your deployment works successfully!</p>
+    </div>
+  );
+}
