51eca3141ba2d462ef571c4d3b3c8b0fa8a500f5
amalkouri2 <amalkouri20@gmail.com>
2025-11-30 01:50:25 +0300

    Delete app directory

--- app/Page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Find App is Running 🚀</h1>
-      <p>Your deployment works successfully!</p>
-    </div>
-  );
-}
